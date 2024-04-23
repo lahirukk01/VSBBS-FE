7ce158b488c2d55aa8274590e542f432fef33225
Lahiru Kariyawasam <lkksoftdev@gmail.com>
2024-04-23 17:42:45 +0530

    Added browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import '~/App.css'
+import '~/App.css';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Landing from '~/pages/Landing.tsx';
 
 const App = () => {
 
   return (
-    <Landing />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </Router>
   );
 };
 
