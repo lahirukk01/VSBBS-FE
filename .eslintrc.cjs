1f388f4a881e9fd1e955c6000fdec089dbbdda9f
Lahiru Kariyawasam <lkksoftdev@gmail.com>
2024-04-23 17:50:33 +0530

    Coding standards updated

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,5 +12,8 @@ module.exports = {
     'react-refresh/only-export-components': 'warn',
     'quotes': ['error', 'single', { 'avoidEscape': true }],
     'semi': ['error', 'always'],
+    'max-lines': ['error', 400],
+    'max-lines-per-function': ['error', 50],
+    'max-len': ['error', { 'code': 120 }],
   },
 }
